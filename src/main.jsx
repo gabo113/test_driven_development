0e712b68163dde9236df79aca524caa3ceea1df5
Gabriel gonzalez <122018691+gabo113@users.noreply.github.com>
2023-11-11 22:19:52 +0100

    fourth commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import { Calculator  } from "./Calculator";
+import React from "react";
+
+createRoot(
+    document.getElementById('root')
+).render(
+    <Calculator />
+)
